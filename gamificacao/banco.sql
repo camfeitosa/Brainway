de0c902a708cdaa2ca2321c7d2936204c054042a
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-07-31 18:49:01 -0300

    bd

--- gamificacao/banco.sql
+++ gamificacao/banco.sql
@@ -5,5 +5,11 @@ USE gamificacao;
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
+	 nivel INT DEFAULT 1,
     pontos INT DEFAULT 0
 );
+
+INSERT INTO usuarios (id, nome, pontos) VALUES (1, 'Camila', DEFAULT);
+
+
+SELECT * FROM usuarios;
