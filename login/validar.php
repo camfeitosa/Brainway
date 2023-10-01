b7c444f2a75e8891bd2c1683cdedc7b54ab925c9
camfeitosa <camilafeitosacoutinho@gmail.com>
2023-10-01 11:38:43 -0300

    google

--- login/validar.php
+++ login/validar.php
@@ -2,6 +2,20 @@
 session_start();
 include('..\conexao.php');
 
+require_once '.../vendor/autoload.php';
+
+// Get $id_token via HTTPS POST.
+
+$client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
+$payload = $client->verifyIdToken($id_token);
+if ($payload) {
+  $userid = $payload['sub'];
+  // If request specified a G Suite domain:
+  //$domain = $payload['hd'];
+} else {
+  // Invalid ID token
+}
+
 // processar o formulário de registro
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   // validar campos
