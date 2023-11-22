d6f058f67fd523f989ea4741ece48429d69456ed
camfeitosa <camilafeitosacoutinho@gmail.com>
2023-11-22 17:04:28 -0300

    notas de texto

--- pages/notas/notas2/script.js
+++ pages/notas/notas2/script.js
@@ -43,9 +43,8 @@ function showNotes() {
   if (!notes) return;
   document.querySelectorAll(".note").forEach((li) => li.remove());
   notes.forEach((note, id) => {
-    if (note) {
-      let filterDesc = note.description.replaceAll("\n", "<br/>");
-      let liTag = `<li class="note">
+    let filterDesc = note.description.replaceAll("\n", "<br/>");
+    let liTag = `<li class="note">
                         <div class="details">
                             <p>${note.title}</p>
                             <div class='desc'>${filterDesc}</div>
@@ -61,115 +60,114 @@ function showNotes() {
                             </div>
                         </div>
                     </li>`;
-      addBox.insertAdjacentHTML("afterend", liTag);
-    }
-  });
-
-  showNotes();
-
-  function showMenu(elem) {
-    elem.parentElement.classList.add("show");
-    document.addEventListener("click", (e) => {
-      if (e.target.tagName != "I" || e.target != elem) {
-        elem.parentElement.classList.remove("show");
-      }
-    });
-  }
-
-  showMenu();
-
-  function deleteNote(noteId) {
-    let confirmDel = confirm("Deseja deletar?");
-    if (!confirmDel) return;
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    showNotes();
-  }
-  deleteNote();
-
-  function updateNote(noteId, title, filterDesc) {
-    let description = filterDesc.replaceAll("<br/>", "\r\n");
-    updateId = noteId;
-    isUpdate = true;
-    addBox.click();
-    titleTag.value = title;
-    descTag.value = description;
-    // popupTitle.innerText = "";
-    addBtn.innerText = "Atualizar nota";
-  }
-  updateNote();
-
-  addBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let title = titleTag.value.trim(),
-      description = descTag.value.trim();
-
-    if (title || description) {
-      let currentDate = new Date(),
-        month = months[currentDate.getMonth()],
-        day = currentDate.getDate(),
-        year = currentDate.getFullYear();
-
-      let noteInfo = { title, description, date: `${month} ${day}, ${year}` };
-      if (!isUpdate) {
-        notes.push(noteInfo);
-      } else {
-        isUpdate = false;
-        notes[updateId] = noteInfo;
-      }
-      localStorage.setItem("notes", JSON.stringify(notes));
-      showNotes();
-      closeIcon.click();
-      saveNoteToServer(noteInfo);
-    }
-  });
-
-  function saveNoteToServer(noteInfo) {
-    const url = "notes.php";
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          // Sucesso na comunicação com o servidor
-          loadNotes(); // Atualiza as notas após o sucesso
-          closeIcon.click();
-        } else {
-          // Tratamento de erro
-          console.error("Erro na comunicação com o servidor");
-        }
-      }
-    };
-
-    const params = `title=${encodeURIComponent(
-      noteInfo.title
-    )}&description=${encodeURIComponent(noteInfo.description)}`;
-    xhr.send(params);
-  }
-
-  // Função para carregar e exibir as notas do usuário
-
-  function loadNotes() {
-    const xhr = new XMLHttpRequest();
-    const url = "load.php"; // Nome do arquivo PHP que recuperará as notas
-
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        const notesContainer = document.getElementById("notes-container");
-        notesContainer.innerHTML = xhr.responseText; // Atualiza o conteúdo do contêiner com as notas
-      }
-    };
-
-    xhr.send();
-  }
-
-  // Chame a função loadNotes() para carregar as notas quando a página for carregada
-  document.addEventListener("DOMContentLoaded", function () {
-    loadNotes();
+    addBox.insertAdjacentHTML("afterend", liTag);
   });
 }
+showNotes();
+
+
+function showMenu(elem) {
+  elem.parentElement.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != elem) {
+      elem.parentElement.classList.remove("show");
+    }
+  });
+}
+
+function deleteNote(noteId) {
+  let confirmDel = confirm("Deseja deletar?");
+  if (!confirmDel) return;
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+}
+
+function updateNote(noteId, title, filterDesc) {
+  let description = filterDesc.replaceAll("<br/>", "\r\n");
+  updateId = noteId;
+  isUpdate = true;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = description;
+  // popupTitle.innerText = "";
+  addBtn.innerText = "Atualizar nota";
+}
+
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let title = titleTag.value.trim(),
+    description = descTag.value.trim();
+
+  if (title || description) {
+    let currentDate = new Date(),
+      month = months[currentDate.getMonth()],
+      day = currentDate.getDate(),
+      year = currentDate.getFullYear();
+
+    let noteInfo = { title, description, date: `${month} ${day}, ${year}` };
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+    closeIcon.click();
+    saveNoteToServer(noteInfo);
+  }
+
+});
+
+function saveNoteToServer(noteInfo) {
+  const url = 'notes.php';
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        // Sucesso na comunicação com o servidor
+        loadNotes(); // Atualiza as notas após o sucesso/        closeIcon.click();
+      } else {
+        // Tratamento de erro
+        console.error('Erro na comunicação com o servidor');
+      }
+    }
+  };
+
+  const params = `title=${encodeURIComponent(noteInfo.title)}&description=${encodeURIComponent(noteInfo.description)}`;
+  xhr.send(params);
+}
+
+// ...
+
+
+
+// Função para carregar e exibir as notas do usuário
+
+
+function loadNotes() {
+  const xhr = new XMLHttpRequest();
+  const url = 'load.php'; // Nome do arquivo PHP que recuperará as notas
+
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const notesContainer = document.getElementById('notes-container');
+      notesContainer.innerHTML = xhr.responseText; // Atualiza o conteúdo do contêiner com as notas
+    }
+  };
+
+  xhr.send();
+}
+
+// Chame a função loadNotes() para carregar as notas quando a página for carregada
+document.addEventListener('DOMContentLoaded', function () {
+  loadNotes();
+});
